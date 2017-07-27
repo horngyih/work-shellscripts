b1b165d7573b3de1ea47fab834be6dc14b2448cd
horngyih <horngyih@yahoo.com>
2017-07-27 15:33:40 +0800

    Added java options to dump TranposrtPipe to log when starting tomcat

--- start-docker-tomcat.sh
+++ start-docker-tomcat.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-sh /d/work/scripts/shellscripts/container-running.sh tomcat-dev;
-running=$?
-
-if [ $running -eq 0 ]
-then
-	echo "Tomcat Dev Docker is already running";
-    runsuccess=0;
-else
+function startTomcatDev
+{
     sh /d/work/scripts/shellscripts/container-exists.sh tomcat-dev;
     exists=$?;
 
@@ -29,18 +23,42 @@ else
     -e MCICO_PATH=/usr/local/tomcat/settings \
    	-e JPDA_ADDRESS=8000 \
     -e JPDA_TRANSPORT=dt_socket \
-    -e JAVA_OPTS="-server -Xms256M -Xmx1024M -XX:PermSize=150m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -Xnoclassgc -Xconcgc -Xmn64m" \
+    -e JAVA_OPTS="-server -Xms256M -Xmx1024M -XX:PermSize=150m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -Xnoclassgc -Xconcgc -Xmn64m -Dcom.sun.xml.ws.transport.local.LocalTransportPipe.dump=true" \
    	tomcat:7 /usr/local/tomcat/bin/catalina.sh jpda run;
     runsuccess=$?;
+}
+
+tail=0;
+restart=0;
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -t | --tail )
+            tail=1;
+            shift
+            ;;
+        -r | --restart )
+            restart=1;
+            shift
+            ;;
+    esac
+done
+
+sh /d/work/scripts/shellscripts/container-running.sh tomcat-dev;
+running=$?
+
+if [ $running -eq 0 ] && [ $restart -eq 0 ]
+then
+	echo "Tomcat Dev Docker is already running";
+    runsuccess=0;
+else
+    startTomcatDev;
 fi
 
-if [ ! -z $1 ]
+if [ $tail -eq 1 ]
 then
     if [ $runsuccess -eq 0 ]
     then
-        if [ $1 = '-t' ]
-        then
-            docker logs -f tomcat-dev;
-        fi
+        docker logs -f tomcat-dev;
     fi
 fi
